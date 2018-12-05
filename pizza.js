751ba0b3642e7659f2a788b5b73778c01146dde0
LAPTOP-DCREO7LQ\luck4you <99046268@mydavinci.nl>
2018-12-05 14:29:29 +0100

    disabled the sql code in javascript

--- pizza.js
+++ pizza.js
@@ -140,23 +140,24 @@ function showPriceForChosenItems()
 	document.getElementById("finalPrice").innerHTML = finalPrice;
 }
 
-try {
-            // Try Connect to the DB with new MySqli object - Params {hostname, userid, password, dbname}
-            $mysqli = new mysqli("localhost", "root", "mysql", "pizza_database");
+//try {
+//            // Try Connect to the DB with new MySqli object - Params {hostname, userid, password, dbname}
+//            $mysqli = new mysqli("localhost", "root", "mysql", "pizza_database");
+//
+//            
+//            $statement = $mysqli->prepare("select pizzaId, pizzaName, pizzaPicture, pizzaPrice from pizzas limit 10");
+//
+//
+//            $statement->execute(); // Execute the statement.
+//            $result = $statement->get_result(); // Binds the last executed statement as a result.
+//
+//            echo json_encode(($result->fetch_assoc())); // Parse to JSON and print.
+//  
+//        } catch (mysqli_sql_exception $e) { // Failed to connect? Lets see the exception details..
+//            echo "MySQLi Error Code: " . $e->getCode() . "<br />";
+//            echo "Exception Msg: " . $e->getMessage();
+//            exit(); // exit and close connection.
+//        }
+//
+//        $mysqli->close(); // finally, close the connection
 
-            
-            $statement = $mysqli->prepare("select pizzaId, pizzaName, pizzaPicture, pizzaPrice from pizzas limit 10");
-
-
-            $statement->execute(); // Execute the statement.
-            $result = $statement->get_result(); // Binds the last executed statement as a result.
-
-            echo json_encode(($result->fetch_assoc())); // Parse to JSON and print.
-  
-        } catch (mysqli_sql_exception $e) { // Failed to connect? Lets see the exception details..
-            echo "MySQLi Error Code: " . $e->getCode() . "<br />";
-            echo "Exception Msg: " . $e->getMessage();
-            exit(); // exit and close connection.
-        }
-
-        $mysqli->close(); // finally, close the connection
