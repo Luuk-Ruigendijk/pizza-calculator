f95a8e6f9ef4f3133607551f70e27bff73b941fc
LAPTOP-DCREO7LQ\luck4you <99046268@mydavinci.nl>
2018-12-05 10:07:36 +0100

    first attempt at combining php and json

--- pizza.js
+++ pizza.js
@@ -136,3 +136,24 @@ function showPriceForChosenItems()
 	finalPrice = allPricesTogether.toFixed(2);
 	document.getElementById("finalPrice").innerHTML = finalPrice;
 }
+
+try {
+            // Try Connect to the DB with new MySqli object - Params {hostname, userid, password, dbname}
+            $mysqli = new mysqli("localhost", "root", "mysql", "pizza_database");
+
+            
+            $statement = $mysqli->prepare("select username, first_name, last_name, gender from users limit 10");
+
+
+            $statement->execute(); // Execute the statement.
+            $result = $statement->get_result(); // Binds the last executed statement as a result.
+
+            echo json_encode(($result->fetch_assoc())); // Parse to JSON and print.
+  
+        } catch (mysqli_sql_exception $e) { // Failed to connect? Lets see the exception details..
+            echo "MySQLi Error Code: " . $e->getCode() . "<br />";
+            echo "Exception Msg: " . $e->getMessage();
+            exit(); // exit and close connection.
+        }
+
+        $mysqli->close(); // finally, close the connection
